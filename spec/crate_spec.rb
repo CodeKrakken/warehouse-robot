c2684b72d5d82e279fc2ec871e336de35f5fc043
CodeKrakken <newholm1@hotmail.com>
2021-01-08 16:54:08 +0000

    Refactor crate spec

--- spec/crate_spec.rb
+++ spec/crate_spec.rb
@@ -2,16 +2,12 @@ require 'crate'
 
 describe Crate do
 
-  let(:robot) { double :robot }
-
   it 'responds to location' do
     expect(subject).to respond_to(:location)
   end
 
   it 'can update its location when dropped by robot' do
-    subject.update([0, 1])
-    expect(subject.location).to eq ([0, 1])
-
+    expect(subject.update([0, 1])).to eq ([0, 1])
   end
 
 end

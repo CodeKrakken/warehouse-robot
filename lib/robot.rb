0bd9b39a5c1f997d478e3289c7cfdd2ec453ebe4
CodeKrakken <newholm1@hotmail.com>
2021-01-10 17:37:03 +0000

    Refactor instruct method

--- lib/robot.rb
+++ lib/robot.rb
@@ -21,29 +21,25 @@ class Robot
       allowed?('w') ? move('w') : 'Cannot move there.'
     when 'ne'
       if @location[1] + 1 <= warehouse.dimensions[1]/2 && @location[0] + 1 <= warehouse.dimensions[0]/2
-        move('n')
-        move('e')
+        move('n') && move('e')
       else
         'Cannot move there.'
       end
     when 'sw'
       if (@location[1] - 1).abs <= warehouse.dimensions[1]/2 && (@location[0] - 1).abs <= warehouse.dimensions[0]/2
-        move('s')
-        move('w')
+        move('s') && move('w')
       else
         'Cannot move there.'
       end
     when 'nw'
       if @location[1] + 1 <= warehouse.dimensions[1]/2 && (@location[0] - 1).abs <= warehouse.dimensions[0]/2
-        move('n')
-        move('w')
+        move('n') && move('w')
       else
         'Cannot move there.'
       end
     when 'se'
       if (@location[1] - 1).abs <= warehouse.dimensions[1]/2 && @location[0] + 1 <= warehouse.dimensions[0]/2
-        move('s')
-        move('e')
+        move('s') && move('e')
       else
         'Cannot move there.'
       end

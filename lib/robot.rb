1914d4a0701fb4633351cb3d8ee41bf7bb9b2659
CodeKrakken <newholm1@hotmail.com>
2021-01-10 20:11:59 +0000

    Abstract East movement

--- lib/robot.rb
+++ lib/robot.rb
@@ -14,19 +14,19 @@ class Robot
     when 'n'
       allowed?('n', :+, 1) ? move(:+, 1) : 'Cannot move there.'
     when 'e'
-      allowed?('e') ? move(:+, 0) : 'Cannot move there.'
+      allowed?('e', :+, 0) ? move(:+, 0) : 'Cannot move there.'
     when 's'
       allowed?('s') ? move(:-, 1) : 'Cannot move there.'
     when 'w'
       allowed?('w') ? move(:-, 0) : 'Cannot move there.'
     when 'ne'
-      allowed?('n', :+, 1) && allowed?('e') ? move(:+, 1) && move(:+, 0) : 'Cannot move there.'
+      allowed?('n', :+, 1) && allowed?('e', :+, 0) ? move(:+, 1) && move(:+, 0) : 'Cannot move there.'
     when 'sw'
       allowed?('s') && allowed?('w') ? move(:-, 1) && move(:-, 0) : 'Cannot move there.'
     when 'nw'
       allowed?('n', :+, 1) && allowed?('w') ? move(:+, 1) && move(:-, 0) : 'Cannot move there.'
     when 'se'
-      allowed?('s') && allowed?('e') ? move(:-, 1) && move(:+, 0) : 'Cannot move there.'
+      allowed?('s') && allowed?('e', :+, 0) ? move(:-, 1) && move(:+, 0) : 'Cannot move there.'
     when 'g'
       grab
     when 'd'
@@ -46,8 +46,8 @@ class Robot
   end
 
   def allowed?(direction, operator=nil, index=nil)
-    return @location[index].send(operator, 1) <= warehouse.dimensions[index]/2 if direction == 'n'
-    return @location[0] + 1 <= warehouse.dimensions[0]/2 if direction == 'e'
+    return @location[index].send(operator, 1) <= warehouse.dimensions[index]/2 if direction == 'n' || direction == 'e'
+    # return @location[0] + 1 <= warehouse.dimensions[0]/2 if direction == 'e'
     return (@location[0] - 1).abs <= warehouse.dimensions[0]/2 if direction == 'w'
     return (@location[1] - 1).abs <= warehouse.dimensions[1]/2 if direction == 's'
   end
